14115bf1800aa006a0c7b0616148bd87b7aaa6b9
Faizan Shabbir <54990877+potato2027@users.noreply.github.com>
2023-06-12 00:36:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const app = express();
 const cors = require('cors');
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
-
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+}) 
 
 app.post('/search', async (req,resp) => {
     
